fb6aa4d1ca081db183a6355586ca02fc8ca39193
Dave R Smith <dave@deb-project.deb-project>
2016-11-26 14:44:56 -0500

    update for newest server

--- xmage_files/startServer.sh
+++ xmage_files/startServer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "${0%/*}"
-java -Xms256M -Xmx512M -XX:MaxPermSize=256m -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-1.4.17.jar
+java -Xms256M -Xmx512M -XX:MaxPermSize=256m -Djava.security.policy=./config/security.policy -Djava.util.logging.config.file=./config/logging.config -Dlog4j.configuration=file:./config/log4j.properties -jar ./lib/mage-server-1.4.18.jar
