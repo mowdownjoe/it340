5018d62d8ec0259f9c7b0065284d8b1edcaa86c1
Dave R Smith <dave@deb-project.deb-project>
2016-12-18 09:37:41 -0500

    fix opening if statements

--- server_files/log_notify.sh
+++ server_files/log_notify.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-if ![[ $2 =~ '^[0-9]+$']]
+if [[ ! $2 =~ ^[0-9]+$ ]];
 then
 	echo "Invalid argument. Syntax log_notify FILE DAYS"
 	exit 1
 fi
-if ![[ -f "$1"]]
+if [ ! -f $1 ];
 then
 	echo "Invalid file path. Syntax log_notify FILE DAYS"
 	exit 1
@@ -16,7 +16,7 @@ then=$(date --date="$2 days ago" +%F)
 
 firstThen=$(grep -n -m 1 $then $1 | cut -f1 -d: )
 lines=$(wc -l $1 | cut -f1 -d: )
-tailLines= $lines - $firstThen
+((tailLines= $lines - $firstThen))
 
 warnings=$(tail --lines=$tailLines $1 | grep "WARN" | wc -l | cut -f1 -d: )
 errors=$(tail --lines=$tailLines $1 | grep "ERROR" | wc -l| cut -f1 -d: )
